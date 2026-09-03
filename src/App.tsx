a56b686f20238958efdf5727333c6a8b70b03c6f
Isaac Carneiro <isaaczoom703@gmail.com>
2026-09-02 22:20:52 -0300

    refactor: remove standalone form route from landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,9 @@ import { LogoStrip } from './components/LogoStrip';
 import { About } from './components/About';
 import { Services } from './components/Services';
 import { Possibilities } from './components/Possibilities';
-import { EventQuoteForm } from './components/EventQuoteForm';
 import { Footer } from './components/Footer';
 
 export default function App() {
-  if (window.location.pathname === '/orcamento' || window.location.pathname === '/orcamento/') {
-    return (
-      <div className="min-h-screen bg-[#f3f3f3] selection:bg-black selection:text-white">
-        <main>
-          <EventQuoteForm />
-        </main>
-      </div>
-    );
-  }
-
   return (
     <div className="min-h-screen bg-[#f3f3f3] selection:bg-black selection:text-white">
       <Navbar />
